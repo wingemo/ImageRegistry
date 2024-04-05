5c1bbe4b68e6cd4ffcfb3a47fe197ae17dfb8729
Wingemo <philip.wingemo@outlook.com>
2024-04-05 23:40:00 +0200

    Update generate_deployment.py

--- script/generate_deployment.py
+++ script/generate_deployment.py
@@ -1,1 +1,45 @@
+import yaml
+
+def generate_deployment(namespace_file):
+    with open(namespace_file, 'r') as file:
+        namespaces = yaml.safe_load(file)
+
+    for service, image in namespaces.items():
+        deployment = {
+            'apiVersion': 'apps/v1',
+            'kind': 'Deployment',
+            'metadata': {
+                'name': service
+            },
+            'spec': {
+                'replicas': 1,
+                'selector': {
+                    'matchLabels': {
+                        'app': service
+                    }
+                },
+                'template': {
+                    'metadata': {
+                        'labels': {
+                            'app': service
+                        }
+                    },
+                    'spec': {
+                        'containers': [{
+                            'name': service,
+                            'image': image,
+                            'ports': [{
+                                'containerPort': 8080
+                            }]
+                        }]
+                    }
+                }
+            }
+        }
+        
+        with open(f'{service}_deployment.yaml', 'w') as deployment_file:
+            yaml.dump(deployment, deployment_file)
+
+if __name__ == "__main__":
+    generate_deployment('namespaces.yaml')
 
